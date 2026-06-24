cfe17b7f62c6e7a549331e3c8ae8a66b971fb45f
Mapetere <mapeterenyasha@gmail.com>
2026-06-24 17:33:01 +0200

    Update portfolio: Remove Plant Triage & NMSolutions, change role to Systems Architect

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ window.addEventListener('load', function () {
 });
 
 // Typewriter effect
-const roles = ['Software Engineer  ', 'Full Stack Developer  ', 'Founder  ', 'Systems Architect  '];
+const roles = ['Systems Architect  ', 'Full Stack Developer  ', 'Founder  '];
 let roleIndex = 0, charIndex = 0, isDeleting = false;
 const typewriter = document.querySelector('.typewriter');
 
